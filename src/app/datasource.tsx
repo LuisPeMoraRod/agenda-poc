1b0b14fd8104b5ac23da480111f27bcf6ca72cd9
LuisPeMoraRod <luismorarod98@estudiantec.cr>
2024-04-22 14:55:14 -0600

    add basic schedule with custom event details

---
+++ src/app/datasource.tsx
@@ -0,0 +1,9 @@
+export let timelineResourceData: Object[] = [
+  {
+    Id: 61,
+    Subject: "Sample event",
+    StartTime: new Date(2024, 3, 23, 15, 30),
+    EndTime: new Date(2024, 3, 23, 16, 30),
+    IsAllDay: false,
+  },
+];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,144 @@
-import Image from "next/image";
+"use client";
+import {
+  Day,
+  Week,
+  WorkWeek,
+  Month,
+  Agenda,
+  ScheduleComponent,
+  ViewsDirective,
+  ViewDirective,
+  EventSettingsModel,
+  ResourcesDirective,
+  ResourceDirective,
+  Inject,
+  Resize,
+  DragAndDrop,
+} from "@syncfusion/ej2-react-schedule";
+import { DateTimePickerComponent } from "@syncfusion/ej2-react-calendars";
+import { DropDownListComponent } from "@syncfusion/ej2-react-dropdowns";
+import { timelineResourceData } from "./datasource";
+import { TreeViewComponent } from "@syncfusion/ej2-react-navigations";
 import styles from "./page.module.css";
 
-export default function Home() {
+// Registering Syncfusion license key
+import { registerLicense } from "@syncfusion/ej2-base";
+registerLicense(
+  "ORg4AjUWIQA/Gnt2UFhhQlJBfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hTX5XdEdjUHpZc3BQRmFU"
+);
+
+export default function Home(this: any) {
+  const eventSettings = {
+    dataSource: timelineResourceData,
+  };
+  const onPopupOpen = (args: {
+    type: string;
+    element: { querySelector: (arg0: string) => any };
+  }) => {
+    if (args.type === "Editor") {
+      let statusElement = args.element.querySelector("#EventType");
+      if (statusElement) {
+        statusElement.setAttribute("name", "EventType");
+      }
+    }
+  };
+  const eventEditorStyle = { width: "100%", cellpadding: "5" };
+  const inputStyle = { width: "100%" };
+  const DescriptionStyle = {
+    width: "100%",
+    height: "60px !important",
+    resize: "vertical",
+  };
+  const editorTemplate = (
+    props:
+      | {
+          EventType: any;
+          startTime: any;
+          StartTime: any;
+          endTime: any;
+          EndTime: any;
+        }
+      | undefined
+  ) => {
+    return props !== undefined ? (
+      <table className="custom-event-editor" style={eventEditorStyle}>
+        <tbody>
+          <tr>
+            <td className="e-textlabel">Nombre</td>
+            <td colSpan={4}>
+              <input
+                id="Summary"
+                placeholder="Escribí el nombre..."
+                className="e-field e-input"
+                type="text"
+                name="Subject"
+                style={inputStyle}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td className="e-textlabel">Entrenador</td>
+            <td colSpan={4}>
+              <DropDownListComponent
+                id="EventType"
+                placeholder="Escogé un entrenador..."
+                data-name="EventType"
+                className="e-field"
+                style={inputStyle}
+                dataSource={["Andrea", "Juan", "Elena"]}
+                value={props.EventType || null}
+              ></DropDownListComponent>
+            </td>
+          </tr>
+          <tr>
+            <td className="e-textlabel">Desde</td>
+            <td colSpan={4}>
+              <DateTimePickerComponent
+                format="dd/MM/yy hh:mm a"
+                id="StartTime"
+                data-name="StartTime"
+                value={new Date(props.startTime || props.StartTime)}
+                className="e-field"
+              ></DateTimePickerComponent>
+            </td>
+          </tr>
+          <tr>
+            <td className="e-textlabel">Hasta</td>
+            <td colSpan={4}>
+              <DateTimePickerComponent
+                format="dd/MM/yy hh:mm a"
+                id="EndTime"
+                data-name="EndTime"
+                value={new Date(props.endTime || props.EndTime)}
+                className="e-field"
+              ></DateTimePickerComponent>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    ) : (
+      <div></div>
+    );
+  };
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div>
+      <div>
+        <h2>PoC - Schedule Component</h2>
+        <ScheduleComponent
+          width="100%"
+          height="550px"
+          currentView="Month"
+          selectedDate={new Date()}
+          eventSettings={eventSettings}
+          editorTemplate={editorTemplate.bind(this)}
+          showQuickInfo={false}
+          popupOpen={onPopupOpen.bind(this)}
+        >
+          <Inject
+            services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}
+          />
+        </ScheduleComponent>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
